d1f0b4c903a81a768c0cc79959bd8b55e4493025
lenovo\lencare <rameshbeandroid@gmail.com>
2018-01-19 14:26:27 +0530

    GitOne toast added...

--- app/src/main/java/example/my/com/mygit/MainActivity.java
+++ app/src/main/java/example/my/com/mygit/MainActivity.java
@@ -2,11 +2,18 @@ package example.my.com.mygit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    displayGitOne();
+
+  }
+
+  private void displayGitOne() {
+    Toast.makeText(this,"GitOne",Toast.LENGTH_SHORT).show();
   }
 }
